f53f72977cbe947a4a8fc3d597f20ccf3bf19cf0
Luis Reche <rsnoverwatch@gmail.com>
2026-08-17 22:10:39 +0200

    chore(desktop): release ClipHub Studio 2.4.25

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -27,8 +27,8 @@ import KillfeedReel from "@/components/killfeed-reel";
 import Reveal from "@/components/reveal";
 
 const DOWNLOAD_URL =
-  "https://github.com/rechedev9/cliphub/releases/download/v2.4.24/ClipHub.Studio.Setup.2.4.24.exe";
-const RELEASE_VERSION = "v2.4.24";
+  "https://github.com/rechedev9/cliphub/releases/download/v2.4.25/ClipHub.Studio.Setup.2.4.25.exe";
+const RELEASE_VERSION = "v2.4.25";
 const REPO_URL = "https://github.com/rechedev9/cliphub";
 
 function Corners() {
