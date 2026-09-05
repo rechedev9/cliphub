c07f63d20636d3a7c755e4755b3a3c314a2f93e1
Luis Reche <rsnoverwatch@gmail.com>
2026-09-05 22:28:01 +0200

    release: point downloads to ClipHub Studio 2.4.57

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -27,8 +27,8 @@ import KillfeedReel from "@/components/killfeed-reel";
 import Reveal from "@/components/reveal";
 
 const DOWNLOAD_URL =
-  "https://github.com/rechedev9/cliphub/releases/download/v2.4.56/ClipHub.Studio.Setup.2.4.56.exe";
-const RELEASE_VERSION = "v2.4.56";
+  "https://github.com/rechedev9/cliphub/releases/download/v2.4.57/ClipHub.Studio.Setup.2.4.57.exe";
+const RELEASE_VERSION = "v2.4.57";
 const REPO_URL = "https://github.com/rechedev9/cliphub";
 
 function Corners() {
