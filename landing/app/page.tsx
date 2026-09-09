797d979051660b985c3e0a25cb31e932866ac40e
Luis Reche <rsnoverwatch@gmail.com>
2026-09-09 11:24:23 +0200

    release: prepare ClipHub Studio 2.4.67

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -27,8 +27,8 @@ import KillfeedReel from "@/components/killfeed-reel";
 import Reveal from "@/components/reveal";
 
 const DOWNLOAD_URL =
-  "https://github.com/rechedev9/cliphub/releases/download/v2.4.66/ClipHub.Studio.Setup.2.4.66.exe";
-const RELEASE_VERSION = "v2.4.66";
+  "https://github.com/rechedev9/cliphub/releases/download/v2.4.67/ClipHub.Studio.Setup.2.4.67.exe";
+const RELEASE_VERSION = "v2.4.67";
 const REPO_URL = "https://github.com/rechedev9/cliphub";
 
 function Corners() {
