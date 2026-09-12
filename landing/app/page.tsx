bf7c515db7fa3d1af6c5538bd636038aa969d0c7
Luis Reche <rsnoverwatch@gmail.com>
2026-09-12 21:14:44 +0200

    chore(release): prepare Studio 3.0.4

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -27,8 +27,8 @@ import KillfeedReel from "@/components/killfeed-reel";
 import Reveal from "@/components/reveal";
 
 const DOWNLOAD_URL =
-  "https://github.com/rechedev9/cliphub/releases/download/v3.0.3/ClipHub.Studio.Setup.3.0.3.exe";
-const RELEASE_VERSION = "v3.0.3";
+  "https://github.com/rechedev9/cliphub/releases/download/v3.0.4/ClipHub.Studio.Setup.3.0.4.exe";
+const RELEASE_VERSION = "v3.0.4";
 const REPO_URL = "https://github.com/rechedev9/cliphub";
 
 function Corners() {
